6ba6d3339984f52d4fd6069ebe3ea312c0f89db4
sonia <sonia.tutaj@gmail.com>
2022-03-30 21:26:18 +0100

    Step 1 + step 2 Depenencies installation, sanity test and watch_mode script

---
+++ src/homeworks/hw-day-1/src/validators/form-validators.spec.js
@@ -0,0 +1,11 @@
+it('should...', () => {
+    // Arrange / Given
+    // const numberA = 10;
+    // const numberB = 20;
+  
+    // Act / When
+    // const result = numberA + numberB;
+  
+    // Assert / Then
+    // expect(result).toBe(30);
+  });
